c45b991de1ffc3ba1b83533baec505e9aaebb41f
NyperYuhgard <93950153+NyperYuhgard@users.noreply.github.com>
2026-07-09 04:45:56 -0500

    Cleaned

--- README.md
+++
@@ -1,5 +0,0 @@
-# Bash
-
-PSXRecomp project for Bash (SCES-02834).
-
-See [PSXRecomp](https://github.com/mstan/psxrecomp).
--- tools/extract_exe.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Extract PS-X EXE from disc image
-python3 psxrecomp/tools/extract_psx_exe.py "/home/nyper/Documentos/GitHub/PSXRecomp_Studio/BashRecomp/bash/bash.cue" SCES_02834 tomba/SCES_02834
